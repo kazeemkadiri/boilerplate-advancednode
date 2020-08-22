38be2352226e08e8646462abc68c93822386c25c
Kazeem Kadiri <kazeem.kadiri@gmail.com>
2020-08-23 00:01:11 +0100

    Added code to pass variables to the pug files from the server

--- server.js
+++ server.js
@@ -6,13 +6,15 @@ const fccTesting = require("./freeCodeCamp/fcctesting.js");
 const app = express();
 
 fccTesting(app); //For FCC testing purposes
+app.set("views", "./views/pug");
+app.set("view engine", 'pug');
 app.use("/public", express.static(process.cwd() + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.route("/").get((req, res) => {
   //Change the response to render the Pug template
-  res.send(`Pug template is not defined.`);
+  res.render("index", {title:"Hello", message:"Please Login"});
 });
 
 app.listen(process.env.PORT || 3000, () => {
