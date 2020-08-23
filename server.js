1f58c690e894820e9de0c145710eb701a8f45f36
Kazeem Kadiri <kazeem.kadiri@gmail.com>
2020-08-23 08:19:46 +0100

    Setup passport serialize and deserialize user object during authentication

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const fccTesting = require("./freeCodeCamp/fcctesting.js");
 const app = express();
 const session = require('express-session');
 const passport = require('passport');
+const ObjectID = require('mongodb').ObjectID;
 
 fccTesting(app); //For FCC testing purposes
 app.set("views", "./views/pug");
@@ -14,7 +15,7 @@ app.use("/public", express.static(process.cwd() + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-console.log(process.env.SESSION_SECRET);
+//console.log(process.env.SESSION_SECRET);
 
 app.use(session({
   secret: process.env.SESSION_SECRET,
@@ -24,6 +25,17 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
+passport.serializeUser((user, done)=>{
+  done(null, user._id);
+});
+
+passport.deserializeUser((id, done)=>{
+  db.collection('users')
+    .findOne({_id: new ObjectID(id)},
+    (err,doc)=>{
+      done(null, doc);
+    });
+});
 app.route("/").get((req, res) => {
   //Change the response to render the Pug template
   res.render("index", {title:"Hello", message:"Please Login"});
