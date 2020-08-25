cfa492474c4a011cf570d8226e869b450d7c24dc
Kazeem Kadiri <kazeem.kadiri@gmail.com>
2020-08-25 15:21:52 +0100

    Removed all routing code

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const LocalStrategy = require("passport-local");
 const ObjectID = require('mongodb').ObjectID;
 const mongo = require('mongodb').MongoClient;
 const bcrypt = require('bcrypt');
-
+const routes = require('./routes');
 
 mongo.connect(process.env.DATABASE,(err, client)=>{
     if(err){ console.log(err);}
@@ -41,42 +41,7 @@ mongo.connect(process.env.DATABASE,(err, client)=>{
         })
        );
          
-        app.route("/register").post((req,res,next)=>{
-         // console.log("In reg route");
-          client.db("users_db").collection("users")
-		.findOne({username:req.body.username},
-	          (err, user)=>{
-                    if(err){
-		     console.log("error");
-                 	 //next(err); 
-                    }
- 		    if(user){ res.redirect("/"); }
-
-                    //Hash password
-                    let hash = bcrypt.hashSync(req.body.password,12);
-
-	   	    client.db("users_db")
-			.collection("users")
-			.insertOne({
-                          username:req.body.username,
-                          password:hash
-                        },(err, doc)=>{
-                          if(err){
-                            res.redirect("/");
-                          }
-                          else{
-
-                            next(null, doc);
-                          }
-                        })
-                  })
-		});
-
-
-	     app.use((req,res,next)=>{
-                res.status(404).type("text")
-	   	   .send("Not Found")
-             });
+	routes(app, client);
 
 	app.listen(process.env.PORT || 3000, () => {
              console.log("Listening on port " + process.env.PORT);
@@ -100,41 +65,4 @@ app.use(session({
 }));
 app.use(passport.initialize());
 app.use(passport.session());
-
-
-app.route("/").get((req, res) => {
-  //Change the response to render the Pug template
-  res.render("index",
-    {
-      title:"Hello",
-      message:"Please Login",
-      showLogin: true,
-      showRegistration: true
-    });
-});
-
-app.post("/login", 
-  passport.authenticate("local",
-    {failureRedirect:"/"}
-  ),
-function(req,res){
-  res.redirect("/profile");
-});
-
-function ensureAuthenticated(req, res,next){
-  if(req.isAuthenticated()){
-    return next();
-  }
-  res.redirect("/");
-}
-                 
-app.route("/profile")
-  .get(ensureAuthenticated, function(req,res,next){
-    res.render("profile",{username:req.user.username});
-})
-
-app.get("/logout",function(req, res){
-  req.logout();
-  res.redirect("/");
-});
 
